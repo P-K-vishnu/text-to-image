48175497857fc6617fa58ce3a137bf49e021cc44
ct176-vishnu-pradeepkumar <ct176-vishnu-pradeepkumar@ct176-vishnu-pradeepkumars-MacBook-Pro.local>
2024-05-03 12:42:47 +0530

    tools UI

--- src/app/pages/homePage.tsx
+++ src/app/pages/homePage.tsx
@@ -356,7 +356,7 @@ export default function Home() {
                 </a>
               </li>
               <li>
-                <a
+                <button
                   onClick={() => {
                     setCurPage(3);
                     setShowmenu(false);
@@ -366,7 +366,7 @@ export default function Home() {
                   } rounded`}
                   aria-current="page">
                   Tools
-                </a>
+                </button>
               </li>
             </ul>
           </div>
@@ -387,7 +387,114 @@ export default function Home() {
             </div>
           </>
         ) : curPage === 3 ? (
-          <></>
+          <>
+            <h1 class="text-xl font-bold text-white capitalize dark:text-white">
+              settings
+            </h1>
+
+            <form>
+              <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+                <div className="z-10 md:z-0">
+                  <label
+                    className="text-white dark:text-gray-200"
+                    for="passwordConfirmation">
+                    Style preset
+                  </label>
+                  <select className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring">
+                    <option>anime</option>
+                    <option>photographic</option>
+                    <option>3d-model</option>
+                    <option>cinematic</option>
+                    <option>digital-art</option>
+                  </select>
+                </div>
+                <div className="z-10 md:z-0">
+                  <label
+                    className="text-white dark:text-gray-200"
+                    for="passwordConfirmation">
+                    Aspet ratio
+                  </label>
+                  <select className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring">
+                    <option>1:1</option>
+                    <option>16:9</option>
+                    <option>21:9</option>
+                    <option>2:3</option>
+                    <option>3:2</option>
+                    <option>4:5</option>
+                    <option>5:4</option>
+                    <option>9:16</option>
+                    <option>9:21</option>
+                  </select>
+                </div>
+                <div className="z-10 md:z-0">
+                  <label
+                    className="text-white dark:text-gray-200"
+                    for="passwordConfirmation">
+                    Seed
+                  </label>
+                  <input
+                    id="range"
+                    type="range"
+                    className="block w-full py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+                  />
+                </div>
+                <div>
+                  <label
+                    className="text-white dark:text-gray-200"
+                    for="passwordConfirmation">
+                    Nagative Prompt
+                  </label>
+                  <textarea
+                    id="textarea"
+                    type="textarea"
+                    className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"></textarea>
+                </div>
+                <div>
+                  <label className="text-white dark:text-gray-200">Image</label>
+                  <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
+                    <div className="space-y-1 text-center">
+                      <svg
+                        className="mx-auto h-12 w-12 text-white"
+                        stroke="currentColor"
+                        fill="none"
+                        viewBox="0 0 48 48"
+                        aria-hidden="true">
+                        <path
+                          d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                          stroke-width="2"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                        />
+                      </svg>
+                      <div className="flex text-sm text-gray-600">
+                        <label
+                          for="file-upload"
+                          className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
+                          <span className=" px-2">Upload a file</span>
+                          <input
+                            id="file-upload"
+                            name="file-upload"
+                            type="file"
+                            className="sr-only"
+                          />
+                        </label>
+                        <p className="pl-1 text-white">or drag and drop</p>
+                      </div>
+                      <p className="text-xs text-white">
+                        PNG, JPG, GIF up to 10MB
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                {/*  */}
+                <div className="flex justify-center items-center">
+                  <button className=" w-full h-fit px-6 py-2 leading-5 text-white transition-colors duration-200 transform  bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    Save
+                  </button>
+                </div>
+              </div>
+            </form>
+          </>
         ) : (
           <>
             {userInput !== '' && genImage !== null ? (
